3b6c9e4789b59a60d5b5c58c876a5dffc122fe49
Savanna Moss <savannamoss9@yahoo.com>
2020-02-23 16:16:21 -0500

    Initial Vue set-up.

---
+++ vue/tapmaps/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Welcome to TapMaps!</h1>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
